7cd2369742cb1b67b122cfddddfa49933f5119fc
WillyandFroggy <willyandfroggy@gmail.com>
2024-04-02 15:39:19 -0400

    Added payment links to comic viewer

--- app/data/comics.ts
+++ app/data/comics.ts
@@ -1,9 +1,9 @@
 let comics = [
-    { "id": "005", "description": "Willy asks Froggy if he is going to the jamboree. Froggy is planning to go, and is especially excited for the fireworks. Willy is frustrated by Froggy's misuse of a common English expression." },
-    { "id": "004", "description": "The fourth comic" },
-    { "id": "003", "description": "The third comic" },
-    { "id": "002", "description": "The second comic" },
-    { "id": "001", "description": "The first comic" }
+    { "id": "005", "description": "Willy asks Froggy if he is going to the jamboree. Froggy is planning to go, and is especially excited for the fireworks. Willy is frustrated by Froggy's misuse of a common English expression.", "variantUrl": "https://shop.willyandfroggy.com/products/bright-red-prints?variant=44821857501428" },
+    { "id": "004", "description": "The fourth comic", "variantUrl": "https://shop.willyandfroggy.com/products/bright-red-prints?variant=44818068308212" },
+    { "id": "003", "description": "The third comic", "variantUrl": "https://shop.willyandfroggy.com/products/bright-red-prints?variant=44818068373748" },
+    { "id": "002", "description": "The second comic", "variantUrl": "https://shop.willyandfroggy.com/products/bright-red-prints?variant=44818068439284" },
+    { "id": "001", "description": "The first comic", "variantUrl": "https://shop.willyandfroggy.com/products/bright-red-prints?variant=44818068504820" }
   ];
 
 export function getLatestComic() {
