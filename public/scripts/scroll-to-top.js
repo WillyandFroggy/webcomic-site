2da593ded6b2aadb07c782677d0f2eba7dfd082c
WillyandFroggy <willyandfroggy@gmail.com>
2024-05-27 13:22:10 -0400

    fixed navbar scroll up on dexktop

--- public/scripts/scroll-to-top.js
+++ public/scripts/scroll-to-top.js
@@ -1,10 +1,12 @@
 var prevScrollpos = window.scrollY;
 window.onscroll = function() {
-    var currentScrollPos = window.scrollY;
-      if (prevScrollpos > currentScrollPos || currentScrollPos <= 0) {
-        document.getElementById("header").style.top = "0";
-      } else {
-        document.getElementById("header").style.top = "-70px";
-      }
-      prevScrollpos = currentScrollPos;
+    if(window.innerWidth <= 920) {
+        var currentScrollPos = window.scrollY;
+        if (prevScrollpos > currentScrollPos || currentScrollPos <= 0) {
+            document.getElementById("header").style.top = "0";
+        } else {
+            document.getElementById("header").style.top = "-70px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
 }
